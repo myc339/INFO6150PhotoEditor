9c118e0796bae94f77da25930b4328dee6564a05
tflearner <18810922130@163.com>
2018-12-10 19:10:58 -0500

    preview

--- PhotoEdit/src/app/preview/preview.component.ts
+++ PhotoEdit/src/app/preview/preview.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PreviewService } from '../preview.service'
 @Component({
   selector: 'app-preview',
   templateUrl: './preview.component.html',
   styleUrls: ['./preview.component.scss']
 })
 export class PreviewComponent implements OnInit {
-
-  constructor() { }
-
+  Img : String;
+  constructor(private previewService : PreviewService) {
+    previewService.getImg.subscribe((value:any)=>{
+      console.log(value);
+      this.Img = value;
+    })
+   }
   ngOnInit() {
   }
-
 }
